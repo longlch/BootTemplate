6459d2a6a569d0fbbb9aed14c4a333436419f238
long <hoanglonglch@gmail.com>
2017-03-15 17:10:51 +0700

    Load marker into map( infowindow not correct)

--- src/main/java/com/guru/controller/MapController.java
+++ src/main/java/com/guru/controller/MapController.java
@@ -1,18 +1,30 @@
 package com.guru.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.guru.model.BusStation;
+import com.guru.service.IMapService;
+import com.guru.service.MapServiceImpl;
+
 @Controller
 @RequestMapping(value="/map")
 public class MapController {
-	
+//	@autowire
+	public static IMapService serviceMap;
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public String home(Model model){
-		model.addAttribute("haha","hios");
+		serviceMap= new MapServiceImpl();
+		List<BusStation> busStations= serviceMap.loadDummyMarker();
+	
+		model.addAttribute("busStations",busStations);
 		return "map";
 	}
+	
+	
 }
--- src/test/java/com/example/TestDirectionWaypoints.java
+++ src/test/java/com/example/TestDirectionWaypoints.java
@@ -1,37 +1,47 @@
 package com.example;
 
 import java.io.FileReader;
+import java.util.ArrayList;
 
 import org.json.JSONArray;
 import org.json.simple.parser.JSONParser;
 
+import com.fasterxml.jackson.core.JsonParser;
 import com.google.maps.DirectionsApi;
 import com.google.maps.GeoApiContext;
 import com.google.maps.model.DirectionsResult;
+import com.guru.model.BusStation;
 
 public class TestDirectionWaypoints {
-	public static void main(String[] args) {
-		JSONParser parser = new JSONParser();
+	public static void main(String[] args) throws Exception {
+//		parse json object(Array) to class Java
+		/*JSONParser parser = new JSONParser();
 		try {
-			Object obj = parser.parse(new FileReader("/home/larry/Desktop/SeniorProject/map/route.js"));
+			Object obj = parser.parse(new FileReader("/home/larry/Desktop/SeniorProject/map/DemoLocation.js"));
 			String jsonString = obj.toString();
 			JSONArray jsonArray = new JSONArray(jsonString);
-			
-			/*for (int i = 0; i < jsonArray.length(); i++) {
-				
-				System.out.println(jsonArray.getJSONObject(i).get("lng"));
-			}*/
-			
-			GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyCv7lDkj6Yd3cMbujJcHTKIo_AzLEga-7c");
-			DirectionsResult result = DirectionsApi.newRequest(context)
-			        .origin("Boston,MA")
-			        .destination("Concord,MA")
-			        .waypoints("Charlestown,MA", "Lexington,MA")
-			.await();
-			System.out.println(result.routes[0].legs[0].distance);
-//			System.out.println(result.routes[0].bounds.northeast.lat);
+			ArrayList<BusStation> busStations= new ArrayList<>();
+			for (int i = 0; i < jsonArray.length(); i++) {
+				BusStation busStation= new BusStation(jsonArray.getJSONObject(i).getString("name"), jsonArray.getJSONObject(i).getDouble("lng"), jsonArray.getJSONObject(i).getDouble("lng"));
+				busStations.add(busStation);
+			}
+			for (BusStation busStation : busStations) {
+				System.out.println(busStation.getLatitude());
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
+		}*/
+		
+		GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyCv7lDkj6Yd3cMbujJcHTKIo_AzLEga-7c");
+		DirectionsResult result = DirectionsApi.newRequest(context)
+				.origin("108.22085169497,16.084605795666")
+				.destination("108.222735,16.0814")
+				.waypoints("108.158338,16.084032")
+				.await();
+//		System.out.println(result.routes[0].legs[0].distance);
+//		System.out.println(result.routes[0].bounds.northeast.lat);
+
+		
+		
 	}
 }
