742b7fecaa803422a9e3bd72a3cdb2ae7edbf990
long <hoanglonglch@gmail.com>
2017-03-31 09:59:35 +0700

    Add info content for markers

--- src/main/resources/static/resources/jsMap.js
+++ src/main/resources/static/resources/jsMap.js
@@ -10,9 +10,9 @@ function closeNav() {
 }
 
 function initMap() {
-	let directionsService = new google.maps.DirectionsService;
+    let directionsService = new google.maps.DirectionsService;
     let directionsDisplay = new google.maps.DirectionsRenderer;
-    
+    let geocoder = new google.maps.Geocoder;
     var styledMapType = new google.maps.StyledMapType(
     [
             {
@@ -69,7 +69,6 @@ function initMap() {
 ], {
             name: 'Styled Map'
         });
-    
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 12
         , center: {
@@ -83,45 +82,43 @@ function initMap() {
     });
     map.mapTypes.set('styled_map', styledMapType);
     map.setMapTypeId('styled_map');
-    
     directionsDisplay.setMap(map);
-    directionsDisplay.setOptions( { suppressMarkers: true } );
-    
+    directionsDisplay.setOptions({
+        suppressMarkers: true
+    });
     let currentRoute;
-    
     /* init event for app */
-    $("body").on("click", ".btn-back", function(event) {
-    	
-    	getBack(url);
+    $("body").on("click", ".btn-back", function (event) {
+        getBack(url);
     });
-    $("body").on("click", ".rowClear", function(event) {
-    		clearMarkers();
-    	  let routeId = $(this).find(".routeId").text();
-    	  ajaxGetContent(url,routeId,"go");
-    	  routeId="route"+routeId;
-    	  currentRoute=routeId;
-    	  callAjax(url, routeId, "go", directionsService, directionsDisplay,map);
+    $("body").on("click", ".rowClear", function (event) {
+        clearMarkers();
+        markers = [];
+        let routeId = $(this).find(".routeId").text();
+        ajaxGetContent(url, routeId, "go");
+        routeId = "route" + routeId;
+        currentRoute = routeId;
+        callAjax(url, routeId, "go", directionsService, directionsDisplay, map,geocoder);
     });
-    $("body").on("click","#btnBack",function(event){
-        	clearMarkers();
-       	 	markers = [];
-            callAjax(url, currentRoute, "back", directionsService, directionsDisplay,map);
+    $("body").on("click", "#btnBack", function (event) {
+        clearMarkers();
+        markers = [];
+        callAjax(url, currentRoute, "back", directionsService, directionsDisplay, map,geocoder);
     });
-    $("body").on("click","#btnGo",function(event){
-    	clearMarkers();
-   	 	markers = [];
-        callAjax(url, currentRoute, "go", directionsService, directionsDisplay,map);
-});
-    
+    $("body").on("click", "#btnGo", function (event) {
+        clearMarkers();
+        markers = [];
+        callAjax(url, currentRoute, "go", directionsService, directionsDisplay, map,geocoder);
+    });
 }
 
-function ajaxGetContent(url, routeId ,trend) {
-	var getUrl = url + "/route/"+routeId;
+function ajaxGetContent(url, routeId, trend) {
+    var getUrl = url + "/route/" + routeId;
     $.ajax({
         type: "GET"
         , url: getUrl
-        ,data:{
-            trend:trend
+        , data: {
+            trend: trend
         }
         , success: function (data) {
             var html = jQuery('<body>').html(data);
@@ -130,10 +127,11 @@ function ajaxGetContent(url, routeId ,trend) {
         }
     });
 }
-function getBack(url){
+
+function getBack(url) {
     $.ajax({
         type: "GET"
-        , url: url 
+        , url: url
         , success: function (data) {
             var html = jQuery('<body>').html(data);
             var content = html.find("#content").html();
@@ -141,69 +139,87 @@ function getBack(url){
         }
     });
 }
+
 function setMapOnAll(map) {
     for (var i = 0; i < markers.length; i++) {
-      markers[i].setMap(map);
+        markers[i].setMap(map);
     }
-  }
+}
+
 function clearMarkers() {
     setMapOnAll(null);
- }
+}
+
 function parseLat(str) {
     let temp = [];
     temp = str.split(',');
     let number = parseFloat(temp[0]);
     return number;
 }
+
 function parseLng(str) {
     let temp = [];
     temp = str.split(',');
     let number = parseFloat(temp[1]);
     return number;
 }
-function calculateAndDisplayRoute1(directionsService, directionsDisplay, jsonResponse,map) {
+
+function callAjax(url, busRoute, trend, directionsService, directionsDisplay, map,geocoder) {
+    $.ajax({
+        type: "GET"
+        , contentType: "application/json"
+        , url: url + "/ajax"
+        , data: {
+            busRoute: busRoute
+            , trend: trend
+        }
+        , dataType: 'json'
+        , timeout: 100000
+        , success: function (jsonResponse) {
+            calculateAndDisplayRoute1(directionsService, directionsDisplay, jsonResponse, map,geocoder);
+        }
+    });
+}
+
+function calculateAndDisplayRoute1(directionsService, directionsDisplay, jsonResponse, map,geocoder) {
     // load waypoint from server
     var totalDataLength = jsonResponse.length;
     var waypts = [];
     var totalDataLength = jsonResponse.length;
-    var latLng = jsonResponse[0].latLng;
     for (let i = 0; i < jsonResponse.length; i++) {
-    	let lat = parseLat(jsonResponse[i].latLng);
+        let lat = parseLat(jsonResponse[i].latLng);
         let lng = parseLng(jsonResponse[i].latLng);
-    	if(i==0){
-    		markers.push(marker = new google.maps.Marker({
-				position: new google.maps.LatLng(lat,lng),
-				icon:"https://chart.googleapis.com/chart?chst=d_map_xpin_icon&chld=pin_star|car-dealer|ADDE63|FF0000",
-				map: map
-		    }));
-    	}
-    	else if (i > 0 && i < jsonResponse.length - 1) {
+        if (i == 0) {
             markers.push(marker = new google.maps.Marker({
-				position: new google.maps.LatLng(lat,lng),
-				icon:"https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=" + (i+1) + "|FF0000|000000",
-				map: map
-		    }));
+                position: new google.maps.LatLng(lat, lng)
+                , icon: "https://chart.googleapis.com/chart?chst=d_map_xpin_icon&chld=pin_star|car-dealer|ADDE63|FF0000"
+                , map: map
+            }));
+        }
+        else if (i > 0 && i < jsonResponse.length - 1) {
+            markers.push(marker = new google.maps.Marker({
+                position: new google.maps.LatLng(lat, lng)
+                , icon: "https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=" + (i + 1) + "|FF0000|000000"
+                , map: map
+            }));
             waypts.push({
                 location: new google.maps.LatLng(lat, lng)
                 , stopover: true
             });
         }
-    	else{
-    		markers.push(marker = new google.maps.Marker({
-				position: new google.maps.LatLng(lat,lng),
-				icon:"https://chart.googleapis.com/chart?chst=d_map_pin_icon&chld=flag|ADDE63",
-				map: map
-		    }));
-    	}
+        else {
+            markers.push(marker = new google.maps.Marker({
+                position: new google.maps.LatLng(lat, lng)
+                , icon: "https://chart.googleapis.com/chart?chst=d_map_pin_icon&chld=flag|ADDE63"
+                , map: map
+            }));
+        }
+        let infowindow = new google.maps.InfoWindow;
+        markers[i].addListener('click', function () {
+            geocodeLatLng(geocoder, map, infowindow, jsonResponse[i].latLng, jsonResponse[i].name);
+            infowindow.open(map, markers[i]);
+        });
     }
-    console.log(waypts[0].location);
-    // cusomize waypoints
-// icon:"https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld=" + i +
-// "|FF0000|000000",
-       for (let i = 0; i < waypts.length; i++) {
-		
-	}  
-    // /////////
     directionsService.route({
         origin: new google.maps.LatLng(parseLat(jsonResponse[0].latLng), parseLng(jsonResponse[0].latLng))
         , destination: new google.maps.LatLng(parseLat(jsonResponse[totalDataLength - 1].latLng), parseLng(jsonResponse[totalDataLength - 1].latLng))
@@ -220,19 +236,26 @@ function calculateAndDisplayRoute1(directionsService, directionsDisplay, jsonRes
     });
 }
 
-function callAjax(url, busRoute, trend, directionsService, directionsDisplay,map) {
-    $.ajax({
-        type: "GET"
-        , contentType: "application/json"
-        , url: url + "/ajax"
-        , data: {
-            busRoute: busRoute
-            , trend: trend
+function geocodeLatLng(geocoder, map, infowindow, latLngObj, nameStation) {
+    let latlngStr = latLngObj.split(',', 2);
+    let latlng = {
+        lat: parseFloat(latlngStr[0])
+        , lng: parseFloat(latlngStr[1])
+    };
+    geocoder.geocode({
+        'location': latlng
+    }, function (results, status) {
+        if (status === 'OK') {
+            if (results[0]) {
+                var contentString = '<div id="infoContent" style="height:85px;width:355px">' + '<div id="siteNotice">' + '</div>' + '<div id="bodyContent" >' + '<p><b>Tên trạm dừng:    </b>' + nameStation + '</p>' + '<p><b>Địa chỉ:    </b>' + results[0].formatted_address + '</p>' + '<input style="height:20px;width:341px" type = "button" value = "Thời gian chờ"/>' + '</div>' + '</div>';
+                infowindow.setContent(contentString);
+            }
+            else {
+                window.alert('No results found');
+            }
         }
-        , dataType: 'json'
-        , timeout: 100000
-        , success: function (jsonResponse) {
-            calculateAndDisplayRoute1(directionsService, directionsDisplay, jsonResponse,map);
+        else {
+            window.alert('Geocoder failed due to: ' + status);
         }
     });
 }
